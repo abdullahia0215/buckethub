e57e57ff228033d0cbdf2a40719fb88dcb5d6440
Abdi <abdullahia0215@gmail.com>
2024-06-18 14:25:56 -0500

    added the cultural store to a table on the culture page

--- src/components/CulturalBrigade/CulturalBrigade.jsx
+++ src/components/CulturalBrigade/CulturalBrigade.jsx
@@ -1,9 +1,13 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 export default function CulturalBrigade() {
   const history = useHistory();
   const dispatch = useDispatch();
+  const cultureBrigade = useSelector(
+    (store) => store.brigadeReducers.cultureReducer
+  );
+  const user = useSelector((store) => store.user);
   useEffect(() => {
     dispatch({
       type: "FETCH_CULTURE",
@@ -15,6 +19,19 @@ export default function CulturalBrigade() {
       <button onClick={() => history.push("/brigades")}>
         Back To Brigades
       </button>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Add To Your Bucket list</th>
+          </tr>
+          {cultureBrigade.map((cultureItem) => (
+            <tr key={cultureItem.id}>
+              <td>{cultureItem.public_bucket_list_item}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
