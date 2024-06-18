01760bba1187ef10e343f50586e028be05cbea9a
Abdi <abdullahia0215@gmail.com>
2024-06-18 14:46:27 -0500

    service brigade now displays the suggestions from the serviceBrigade store

--- src/components/ServiceBrigade/ServiceBrigade.jsx
+++ src/components/ServiceBrigade/ServiceBrigade.jsx
@@ -1,20 +1,38 @@
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 export default function ServiceBrigade() {
   const dispatch = useDispatch();
   const history = useHistory();
+  const serviceBrigade = useSelector(
+    (store) => store.brigadeReducers.serviceReducer
+  );
+  const user = useSelector((store) => store.user);
   useEffect(() => {
     dispatch({
-      type: 'FETCH_SERVICE'
-    })
-  }, [])
+      type: "FETCH_SERVICE",
+    });
+  }, []);
   return (
     <>
       <h1>Service</h1>
       <button onClick={() => history.push("/brigades")}>
         Back To Brigades
       </button>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Add To Your Bucket list</th>
+          </tr>
+          {serviceBrigade.map((serviceItem) => (
+            <tr key={serviceItem.id}>
+              <td>{serviceItem.public_bucket_list_item}</td>
+              {user.id === serviceItem.user_id ? <button>Delete</button> : ""}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
